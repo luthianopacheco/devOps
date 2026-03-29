78abdb895167439f1df1596799eb83d4f37533db
luthianopacheco <luthianopacheco@gmail.com>
2026-03-28 21:21:29 -0300

    ajuste terraform

--- infra/main.tf
+++ infra/main.tf
@@ -32,14 +32,14 @@ resource "aws_security_group" "app_security" {
 }
 
 # Chave SSH
-resource "aws_key_pair" "tf_key_pair" {
-  key_name   = "tf-key-pair"
-  public_key = tls_private_key.rsa.public_key_openssh
-}
-
 resource "tls_private_key" "rsa" {
   algorithm = "RSA"
   rsa_bits  = 4096
+}
+
+resource "aws_key_pair" "tf_key_pair" {
+  key_name   = "tf-key-pair"
+  public_key = tls_private_key.rsa.public_key_openssh
 }
 
 resource "local_file" "tf_key" {
